eb763207754e26bcd0397db24aaa7cf88f0027bf
Hitesh <Hiteshrew123@gmail.com>
2020-03-02 15:58:38 +0530

    initail commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var requestURL;
+let request = new new XMLHttpRequest();
+
+function choose1(choice) {
+	// body...
+	switch (choice) {
+	 	case 'movies':
+	 		// statements_1
+	 		requestURL = '';
+	 		break;
+	 	
+	 	default:
+	 		// statements_def
+	 		break;
+	 } 
+
+	
+}
